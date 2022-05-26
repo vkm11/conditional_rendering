3d19409c8dd4ef2a0628ce1301f86de3ea93228a
vijaykumar <vijay1111mane@gmail.com>
2022-05-26 17:05:03 +0530

    commit;

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2 v-if="num === 0">The number is zero</h2>
+  <h2 v-else-if="num < 0">The number is negative</h2>
+  <h2 v-else-if="num > 0">The number is positive</h2>
+  <h2 v-else>Not a number</h2>
+
+  <!-- <div v-if="display">
+  <h4>Vijaykumar</h4>
+  <h4>Prajwal</h4>
+  <h4>Nithin</h4>
+  </div> -->
+
+  <!-- OR -->
+
+  <template v-if="display">
+    <h4>Vijaykumar</h4>
+    <h4>Prajwal</h4>
+    <h4>Nithin</h4>
+  </template>
+
+<h4 v-show="showElement">Using v-show</h4>
+<h4 v-if="showElement">Using v-if</h4>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      // num:'v'  // output:- not a number
+      // num:1  // output:- The number is positive
+      num: -1, //output:- The number is negative
+      display: true,
+      showElement: true,
+    };
+  },
+};
 </script>
 
 <style>
